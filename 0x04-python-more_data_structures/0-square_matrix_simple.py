e3c2099e755176f67411aec7230c70544dd84583
olamideolayemi <olamid15@gmail.com>
2023-12-07 12:00:55 -0800

    updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-	"""
-	first method using map an lambda
-
-	lamb = lambda x: x**2
-	return list(map(lambda row: list(map(lamb, row)), matrix))
-
-	second method below using list comprehension
-	"""
-	return [[x**2 for x in row] for row in matrix]
+    return [[x**2 for x in row] for row in matrix]
