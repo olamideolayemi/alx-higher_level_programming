65f94fb70f5b3f63146cbf436dbb7c8462c52b6e
olamideolayemi <olamid15@gmail.com>
2024-01-08 18:47:26 -0800

    corrected

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
 """
-Ths module contains functions that multiply 2 matrices together using the numpy
-module
+Module 101-lazy_matrix-mul
+Ths module contains functions that multiply 2 matrices together
+using the numpy module
 """
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """
-    Return matrix multiplied together using numpy
-    
+    """Return matrix multiplied together using numpy
     Args:
         m_a: matrix a
         m_b: matrix b
