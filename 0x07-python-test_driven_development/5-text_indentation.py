cd28903bfcaedc5eb97c898c9457d0f281ed6eee
olamideolayemi <olamid15@gmail.com>
2024-01-08 18:41:07 -0800

    corrected

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 
 """
-This module contains function that adds identation to text by printing a text
-with 2 new linex after each of these characters ".", "?" and ":"
+This module contains function that adds identation to text by
+printing a text with 2 new linex after each of these characters
+".", "?" and ":"
+
 """
 
 
 def text_indentation(text):
-    """
-    This function accepts a string and prints a text with 2 new lines
-    after each of these characters ".", "?" and ":"
+    """This function accepts a string and prints a text with
+    2 new lines after each of these characters ".", "?" and ":"
 
     Args:
         text: The string to be supplied
 
     Raise:
         TypeError: text must be a string
-    """
 
+    """
     delim = (".", "?", ":")
 
     if not isinstance(text, str):
