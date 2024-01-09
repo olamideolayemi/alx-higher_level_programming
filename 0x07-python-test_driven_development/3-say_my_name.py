fe393951024938955c9cd17d74db3205a6162363
olamideolayemi <olamid15@gmail.com>
2024-01-08 18:36:36 -0800

    corrected

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,11 +4,12 @@
 This module contains function that prints the name(first and last name)
 of the argument supplied to it. The data type of both arguments have to be
  a string and if otherwise, it throws and error.
-"""
+
+ """
+
 
 def say_my_name(first_name, last_name=""):
-    """
-    This function prints the name(first and last) supplied
+    """This function prints the name(first and last) supplied
     to it.
 
     Args:
@@ -17,6 +18,7 @@ def say_my_name(first_name, last_name=""):
 
     Raise:
         TypeError: first_name must be a string or last_name must be a string
+
     """
 
     if not isinstance(first_name, str):
